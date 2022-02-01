b010645e5b46c0c28a5c04049ce3f578150430e2
Sankarshana V <61644684+limsanky@users.noreply.github.com>
2022-02-01 20:12:02 +0900

    Create Astronaut struct

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Sankarshana V on 2022/02/01.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    var id: String
+    let name: String
+    let description: String
+}
