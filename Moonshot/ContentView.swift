677fd5f736891f5efcf6e20c37181c14ea457529
Sankarshana V <61644684+limsanky@users.noreply.github.com>
2022-02-01 20:17:09 +0900

    Add information to content view

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,8 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
     var body: some View {
-        Text("Hello, world!")
+        Text("\(missions.count)")
             .padding()
     }
 }
