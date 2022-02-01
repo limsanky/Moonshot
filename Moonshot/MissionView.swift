e7c393e5e0d2aacfe40da3e95fe0160cb1f34680
Sankarshana V <61644684+limsanky@users.noreply.github.com>
2022-02-01 20:50:01 +0900

    Create MissionView

---
+++ Moonshot/MissionView.swift
@@ -0,0 +1,48 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Sankarshana V on 2022/02/01.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    let mission: Mission
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView {
+                VStack {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: geometry.size.width * 0.6)
+                        .padding(.top)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Mission Highlights")
+                            .font(.title.bold())
+                            .padding(.bottom, 5)
+                        
+                        Text(mission.description)
+                    }
+                    .padding(.horizontal)
+                }
+                .padding(.bottom)
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct MissionView_Previews: PreviewProvider {
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    static var previews: some View {
+        MissionView(mission: missions[0])
+            .preferredColorScheme(.dark)
+    }
+}
