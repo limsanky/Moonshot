20620a0bea584e391b4917ce7a38d3ea569a42ca
Sankarshana V <61644684+limsanky@users.noreply.github.com>
2022-02-01 20:12:21 +0900

    Create Mission struct
    It contains nested struct CrewRole

---
+++ Moonshot/Mission.swift
@@ -0,0 +1,20 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Sankarshana V on 2022/02/01.
+//
+
+import Foundation
+
+struct Mission: Codable & Identifiable {
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+    
+    struct CrewRole: Codable {
+        let name: String?
+        let role: String
+    }
+}
